447bee61520e6ec5a61c75e25a4b755452d65836
Isaac Carnes <isaacjcarnes@gmail.com>
2023-04-21 18:01:54 -0400

    Added router

--- src/js/App.js
+++ src/js/App.js
@@ -1,13 +1,21 @@
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+
 import '../css/App.css';
 import Header from './Header';
 import Gallery from './Gallery';
 
 function App() {
+  const basePath = "/aimage-gallery/";
   return (
     <div className="App">
       <Header />
       <div id='Content'>
-        <Gallery />
+        <Routes>
+        <Route exact path={basePath} element={<Gallery />}/>
+        </Routes>
       </div>
     </div>
   );
