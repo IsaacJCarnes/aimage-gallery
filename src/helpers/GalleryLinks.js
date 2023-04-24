e35ddc81f7591f100951ce5ee1eb3cf0964c12e6
Isaac Carnes <isaacjcarnes@gmail.com>
2023-04-24 19:35:15 -0400

    Moved away from css grid and instead starting using flexbox for a more dynamic photo grid. Other CSS changes as well

--- src/helpers/GalleryLinks.js
+++ src/helpers/GalleryLinks.js
@@ -1,39 +1,55 @@
 const galleryLinks = [
   {
     link: "https://cdn.midjourney.com/8ba7e39c-4cfd-43ae-b7b3-6b7e65a9239a/grid_0.webp",
+    ar: "1:2",
   },
   {
     link: "https://cdn.midjourney.com/2b4e2d13-d96d-4e4a-b9e5-a43bfe6e8021/0_3.webp",
+    ar: "1:2",
   },
   {
     link: "https://cdn.midjourney.com/14b8244e-8ef9-4676-96cf-23cf06bbdba1/0_0.webp",
+    ar: "1:2",
   },
   {
     link: "https://cdn.midjourney.com/bc3b398c-e383-4f7a-a783-033935ce534e/0_0.webp",
+    ar: "1:2",
   },
   {
     link: "https://cdn.midjourney.com/e1dabafc-c089-4339-b203-2b9397948dde/0_2.webp",
+    ar: "1:2",
   },
   {
     link: "https://cdn.midjourney.com/f29b0994-c070-4256-98c4-c03a7d8b1450/0_3.webp",
+    ar: "1:2",
   },
   {
     link: "https://cdn.midjourney.com/2417ee2c-39c1-4391-93dc-044ac6e0dcc9/0_3.webp",
+    ar: "1:2",
   },
   {
     link: "https://cdn.midjourney.com/61a14704-1a09-4f45-aacd-0889a5ae0a3c/0_3.webp",
+    ar: "1:2",
   },
   {
     link: "https://cdn.midjourney.com/9bdaec99-4a08-4679-99d2-83b5abdceea0/grid_0.webp",
+    ar: "1:2",
   },
   {
     link: "https://cdn.midjourney.com/7e5938e9-87f9-41c9-b148-b293d57bde83/0_2.webp",
+    ar: "1:2",
   },
   {
     link: "https://cdn.midjourney.com/4d120f9d-e707-4ffb-a9bf-aad5223bf3b7/0_3.webp",
+    ar: "1:2",
   },
   {
     link: "https://cdn.midjourney.com/18d1dcc4-e39e-43f5-932f-66824e37bda6/0_3.webp",
+    ar: "1:2",
+  },
+  {
+    link: "https://cdn.midjourney.com/7ba5f97d-0f8c-4ac2-968f-86c037a4a469/0_0.webp",
+    ar: "1:1",
   },
 ];
 
--- src/js/Gallery.js
+++ src/js/Gallery.js
@@ -3,8 +3,15 @@ import {galleryLinks} from '../helpers/GalleryLinks';
 
 function Gallery() {
   const galleryImages = galleryLinks.map((imageData, i) => {
-    return <div className='galleryFrame' key={'galleryImage'+i}><img className='galleryImage' alt='' src={imageData.link}/></div>
-  });  
+    if(imageData.ar === "1:2"){ //Tall images
+      return <div className='galleryFrame tallImage' key={'galleryImage'+i}><img className='galleryImage' alt='' src={imageData.link}/></div>
+    } else if(imageData.ar === "2:1"){ //Wide images
+      return <div className='galleryFrame wideImage' key={'galleryImage'+i}><img className='galleryImage' alt='' src={imageData.link}/></div>
+    } else { //Square images
+      return <div className='galleryFrame squareImage' key={'galleryImage'+i}><img className='galleryImage' alt='' src={imageData.link}/></div>
+    }
+  });
+
   return (
     <div id="Gallery">
         {galleryImages}   
