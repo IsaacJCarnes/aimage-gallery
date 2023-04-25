0e48a6d9f15e317522c08e41a6f666094d7afb6a
Isaac Carnes <isaacjcarnes@gmail.com>
2023-04-24 21:17:03 -0400

    Added lazy loading with blur effect on load

--- src/js/Gallery.js
+++ src/js/Gallery.js
@@ -1,14 +1,18 @@
 import '../css/Gallery.css';
 import {galleryLinks} from '../helpers/GalleryLinks';
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
+
 
 function Gallery() {
   const galleryImages = galleryLinks.map((imageData, i) => {
     if(imageData.ar === "1:2"){ //Tall images
-      return <div className='galleryFrame tallImage' key={'galleryImage'+i}><img className='galleryImage' alt='' src={imageData.link}/></div>
+      return <div className='galleryFrame tallImage' key={'galleryImage'+i}><LazyLoadImage className='galleryImage' alt='' src={imageData.link} effect='blur'/></div>
     } else if(imageData.ar === "2:1"){ //Wide images
-      return <div className='galleryFrame wideImage' key={'galleryImage'+i}><img className='galleryImage' alt='' src={imageData.link}/></div>
+      return <div className='galleryFrame wideImage' key={'galleryImage'+i}><LazyLoadImage className='galleryImage' alt='' src={imageData.link} effect='blur'/></div>
     } else { //Square images
-      return <div className='galleryFrame squareImage' key={'galleryImage'+i}><img className='galleryImage' alt='' src={imageData.link}/></div>
+      return <div className='galleryFrame squareImage' key={'galleryImage'+i}><LazyLoadImage className='galleryImage' alt='' src={imageData.link} effect='blur'/></div>
     }
   });
 
