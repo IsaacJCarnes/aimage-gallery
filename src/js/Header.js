3af3aca9b56cdd02d5842fca4afa7c006c5e6dcb
Isaac Carnes <isaacjcarnes@gmail.com>
2023-04-26 22:38:28 -0400

    Added fun glitch style animation to website title

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import '../css/Header.css';
-
 function Header() {
+  const usableNames = ["AImage Gallery", "A Image Gallery"]
+  const funNames = ["AI Mage Gallery", "AImageGallery"]
+  //Usable names should show up twice as much as fun names
+  const possibleNames = usableNames.concat(usableNames).concat(funNames);
+  const [name, setName] = useState("AImage Gallery");
   return (
     <div id="Header">
         <div id='HeaderArea'>
-            <div id='WebName'>AImage Gallery</div>
+            <div id='WebName' onAnimationIteration={(e) => {e.preventDefault(); setName(possibleNames[Math.floor(Math.random() * possibleNames.length)])}}>{name}</div>
         </div>
     </div>
   );
