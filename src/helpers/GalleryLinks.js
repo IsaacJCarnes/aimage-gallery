523a4cf790363eec68992256c7a97bb9331d0ddd
Isaac Carnes <isaacjcarnes@gmail.com>
2023-05-04 23:43:55 -0400

    Added html basic lazy loading and restructured image array

--- src/helpers/GalleryLinks.js
+++ src/helpers/GalleryLinks.js
@@ -2,86 +2,107 @@ const galleryLinks = [
   {
     link: "https://cdn.midjourney.com/8ba7e39c-4cfd-43ae-b7b3-6b7e65a9239a/grid_0.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/2b4e2d13-d96d-4e4a-b9e5-a43bfe6e8021/0_3.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/14b8244e-8ef9-4676-96cf-23cf06bbdba1/0_0.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/bc3b398c-e383-4f7a-a783-033935ce534e/0_0.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/e1dabafc-c089-4339-b203-2b9397948dde/0_2.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/f29b0994-c070-4256-98c4-c03a7d8b1450/0_3.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/2417ee2c-39c1-4391-93dc-044ac6e0dcc9/0_3.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/61a14704-1a09-4f45-aacd-0889a5ae0a3c/0_3.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/9bdaec99-4a08-4679-99d2-83b5abdceea0/grid_0.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/7e5938e9-87f9-41c9-b148-b293d57bde83/0_2.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/4d120f9d-e707-4ffb-a9bf-aad5223bf3b7/0_3.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/18d1dcc4-e39e-43f5-932f-66824e37bda6/0_3.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/7ba5f97d-0f8c-4ac2-968f-86c037a4a469/0_0.webp",
     ar: "1:1",
+    imgType: "squareImage",
   },
   {
     link: "https://cdn.midjourney.com/82efaefd-c36b-4788-9690-91519227f2cb/0_1.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/7a73d338-3cba-4165-a45f-587322dee698/0_0.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/ca7763f7-f31d-4090-bd1b-118dc6e6dc14/0_1.webp",
     ar: "1:1",
+    imgType: "squareImage",
   },
   {
     link: "https://cdn.midjourney.com/48a8db31-ff99-464f-b7dc-e2275fb4b048/0_0.webp",
     ar: "1:1",
+    imgType: "squareImage",
   },
   {
     link: "https://cdn.midjourney.com/d11a8775-3920-4713-9c4c-90d095bcdfe7/grid_0.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/71eb021d-17a0-4077-8e89-ce492a07a425/grid_0.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/fcee5f24-3134-4fe9-98df-0ac834935c82/grid_0.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
   {
     link: "https://cdn.midjourney.com/1838b2d0-5385-46d3-8aa0-0d0638a73e75/grid_0.webp",
     ar: "1:2",
+    imgType: "tallImage",
   },
 ];
 
--- src/js/Gallery.js
+++ src/js/Gallery.js
@@ -1,58 +1,30 @@
 import "../css/Gallery.css";
 import { galleryLinks } from "../helpers/GalleryLinks";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
+//import { LazyLoadImage } from "react-lazy-load-image-component";
+//import "react-lazy-load-image-component/src/effects/blur.css";
 import { useEffect, useState, useMemo } from "react";
-import { Suspense } from "react";
 
 function Gallery() {
   const [photoData, setPhotoData] = useState([]);
 
   const [isPictureChosen, setIsPictureChosen] = useState(false);
-  const [chosenImage, setChosenImage] = useState("");
-  const [chosenImageType, setChosenImageType] = useState("");
-
-
+  const [chosenImage, setChosenImage] = useState(0);
 
   const galleryImages = useMemo(() => photoData.map((imageData, i) => {
-    //Square images by default
-    let imgType = "squareImage";
-    if (imageData.ar === "1:2") {
-      //Tall images
-      imgType = "tallImage";
-    } else if (imageData.ar === "2:1") {
-      //Wide images
-      imgType = "wideImage";
-    }
     return (
-      <div className={"galleryFrame " + imgType} key={"galleryImage" + i}>
+      <div className={"galleryFrame " + imageData.imgType} key={"galleryImage" + i}>
           <img
             className="galleryImage"
+            loading="lazy"
             alt=""
             src={imageData.link}
-            effect="blur"
-            onClick={(e) => {e.preventDefault(); setChosenImage(imageData.link); setChosenImageType(imgType); setIsPictureChosen(true)}}
+            onClick={(e) => {e.preventDefault(); setChosenImage(i); setIsPictureChosen(true)}}
           />
       </div>
     );
   }), [photoData]);
 
-  /*
-  return (
-      <div className={"galleryFrame " + imgType} key={"galleryImage" + i}>
-        <Suspense fallback={<div className="galleryImage"></div>}>
-          <LazyLoadImage
-            className="galleryImage"
-            alt=""
-            src={imageData.link}
-            effect="blur"
-          />
-        </Suspense>
-      </div>
-    );
-    */
-
-  useEffect(() => {
+  useEffect(() => { //Randomize photo array on page load
     var tempArr = galleryLinks;
     for (var i = tempArr.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
@@ -63,7 +35,7 @@ function Gallery() {
     setPhotoData(tempArr);
   }, []);
 
-  return <div id="PictureContent"><div id="Gallery">{galleryImages}</div><div id="LargeSpace" onClick={(e) => {e.preventDefault(); if(e.target.id === "LargeSpace")setIsPictureChosen(false)}} className={(isPictureChosen ? "visibleElem":"invisibleElem")}><div id="LargeFrame" className={chosenImageType}><div id="TitleTab"><div id="CloseBtn" onClick={(e) => {e.preventDefault(); setIsPictureChosen(false);}}>X</div></div><img alt="Chosen Large Image" id="LargeImage" className={chosenImageType} src={chosenImage}/></div></div></div>;
+  return <div id="PictureContent"><div id="Gallery">{galleryImages}</div><div id="LargeSpace" onClick={(e) => {e.preventDefault(); if(e.target.id === "LargeSpace")setIsPictureChosen(false)}} className={(isPictureChosen ? "visibleElem":"invisibleElem")}><div id="LargeFrame" className={(isPictureChosen ? photoData[chosenImage].imgType : "")}><div id="TitleTab"><div id="CloseBtn" onClick={(e) => {e.preventDefault(); setIsPictureChosen(false);}}>X</div></div><img alt="Chosen Large Image" id="LargeImage" className={(isPictureChosen ? photoData[chosenImage].imgType:"")} src={(isPictureChosen ? photoData[chosenImage].link : "")}/></div></div></div>;
 }
 
 export default Gallery;
