1be4b6d8746dffd273bd7a48b0d4b7d580b0bb2f
Isaac Carnes <isaacjcarnes@gmail.com>
2023-05-12 20:25:34 -0400

    Updated picture storage with more pictures and made twoThirdsImage size

--- src/helpers/GalleryLinks.js
+++ src/helpers/GalleryLinks.js
@@ -1,109 +1,360 @@
 const galleryLinks = [
   {
-    link: "https://cdn.midjourney.com/8ba7e39c-4cfd-43ae-b7b3-6b7e65a9239a/grid_0.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/5d8daed5-095e-430d-88db-40fb16db24f9/0_0.webp",
+      version: 5.1,
+      ar: "1:1",
+      size: "1024x1024",
+      imgType: "squareImage",
+      date: "5/11/2023"
   },
   {
-    link: "https://cdn.midjourney.com/2b4e2d13-d96d-4e4a-b9e5-a43bfe6e8021/0_3.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/5ef2191c-63b5-4909-8e55-f82f4a3b6844/0_3.webp",
+      version: "5.1 raw",
+      ar: "1:1",
+      size: "1024x1024",
+      imgType: "squareImage",
+      date: "5/4/2023"
   },
   {
-    link: "https://cdn.midjourney.com/14b8244e-8ef9-4676-96cf-23cf06bbdba1/0_0.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/0ecfeca8-ba82-4341-bffc-c7476228b7c9/0_2.webp",
+      version: 5,
+      ar: "1:1",
+      size: "1024x1024",
+      imgType: "squareImage",
+      date: "5/3/2023"
   },
   {
-    link: "https://cdn.midjourney.com/bc3b398c-e383-4f7a-a783-033935ce534e/0_0.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/a2eb61d1-ac8d-493d-abef-12ad71641afd/0_3.webp",
+      version: 5,
+      ar: "1:1",
+      size: "1024x1024",
+      imgType: "squareImage",
+      text: "Creature sitting in a cave",
+      date: "5/3/2023"
   },
   {
-    link: "https://cdn.midjourney.com/e1dabafc-c089-4339-b203-2b9397948dde/0_2.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/8b1ca3e7-ef31-4015-bd81-0219cd8930b3/0_1.webp",
+      version: 5,
+      ar: "1:1",
+      size: "1024x1024",
+      imgType: "squareImage",
+      date: "5/3/2023"
   },
   {
-    link: "https://cdn.midjourney.com/f29b0994-c070-4256-98c4-c03a7d8b1450/0_3.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/3dea0bf8-b0cc-47a3-9171-104f857186e8/0_0.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "A cat driving a cat shaped bipedal mech, anime style, smooth lighting",
+      date: "4/11/2023"
   },
   {
-    link: "https://cdn.midjourney.com/2417ee2c-39c1-4391-93dc-044ac6e0dcc9/0_3.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/0075ac97-ae59-4c15-b5ea-37aab5506211/0_3.webp",
+      version: 5,
+      ar: "1:1",
+      size: "1024x1024",
+      imgType: "squareImage",
+      text: "An eye of godzilla peering at you through your square window, square window, digital art",
+      date: "4/24/2023"
   },
   {
-    link: "https://cdn.midjourney.com/61a14704-1a09-4f45-aacd-0889a5ae0a3c/0_3.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/65f88a5e-94a4-4510-88ff-353a843d8e69/0_3.webp",
+      version: 5,
+      ar: "1:1",
+      size: "1024x1024",
+      imgType: "squareImage",
+      text: "The giant eye of a monster looking at you peeking through your window, digital art, toon shaders",
+      date: "4/24/2023"
   },
   {
-    link: "https://cdn.midjourney.com/9bdaec99-4a08-4679-99d2-83b5abdceea0/grid_0.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/ca7763f7-f31d-4090-bd1b-118dc6e6dc14/0_1.webp",
+      version: 5,
+      ar: "1:1",
+      size: "1024x1024",
+      imgType: "squareImage",
+      text: "The giant eye of a monster looking at you through your window, digital art, toon shader",
+      date: "4/24/2023"
   },
   {
-    link: "https://cdn.midjourney.com/7e5938e9-87f9-41c9-b148-b293d57bde83/0_2.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/7a73d338-3cba-4165-a45f-587322dee698/0_0.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "A man facing forwards with a crt television where his head should be, photorealistic, portrait",
+      date: "4/24/2023"
   },
   {
-    link: "https://cdn.midjourney.com/4d120f9d-e707-4ffb-a9bf-aad5223bf3b7/0_3.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/82efaefd-c36b-4788-9690-91519227f2cb/0_1.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "A space traveler wearing a cloak, in a futuristic tavern",
+      date: "4/24/2023"
   },
   {
-    link: "https://cdn.midjourney.com/18d1dcc4-e39e-43f5-932f-66824e37bda6/0_3.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/7ba5f97d-0f8c-4ac2-968f-86c037a4a469/0_0.webp",
+      version: 5,
+      ar: "1:1",
+      size: "1024x1024",
+      imgType: "squareImage",
+      text: "Trippy texture, bright colors, psychedelic texture",
+      date: "4/20/2023"
   },
   {
-    link: "https://cdn.midjourney.com/7ba5f97d-0f8c-4ac2-968f-86c037a4a469/0_0.webp",
-    ar: "1:1",
-    imgType: "squareImage",
+      link: "https://cdn.midjourney.com/14b8244e-8ef9-4676-96cf-23cf06bbdba1/0_0.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "A dark laboratory with body bags hanging from the ceiling, a man stands in front of his desk, red colors, digital art",
+      date: "4/18/2023"
   },
   {
-    link: "https://cdn.midjourney.com/82efaefd-c36b-4788-9690-91519227f2cb/0_1.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/8ba7e39c-4cfd-43ae-b7b3-6b7e65a9239a/grid_0.webp",
+      version: 5,
+      ar: "1:2",
+      size: "864x1728",
+      imgType: "tallImage",
+      text: "Multicolored glowing lights made out of fluid suspended from the ceiling, digital art",
+      date: "4/18/2023"
   },
   {
-    link: "https://cdn.midjourney.com/7a73d338-3cba-4165-a45f-587322dee698/0_0.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/bc3b398c-e383-4f7a-a783-033935ce534e/0_0.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "Old worn out sneakers on a step in front of a brick apartment building, digital art",
+      date: "4/14/2023"
   },
   {
-    link: "https://cdn.midjourney.com/ca7763f7-f31d-4090-bd1b-118dc6e6dc14/0_1.webp",
-    ar: "1:1",
-    imgType: "squareImage",
+      link: "https://cdn.midjourney.com/e1dabafc-c089-4339-b203-2b9397948dde/0_2.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "A fluffy small kitten sitting on the floor pawing at a ray of sunlight, photorealistic, ambient lighting",
+      date: "4/13/2023"
   },
   {
-    link: "https://cdn.midjourney.com/48a8db31-ff99-464f-b7dc-e2275fb4b048/0_0.webp",
-    ar: "1:1",
-    imgType: "squareImage",
+      link: "https://cdn.midjourney.com/f29b0994-c070-4256-98c4-c03a7d8b1450/0_3.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "Sheep lined up on a conveyer belt being fed into an open incinerator with visible flames, by Allie Brosh",
+      date: "4/13/2023"
   },
   {
-    link: "https://cdn.midjourney.com/d11a8775-3920-4713-9c4c-90d095bcdfe7/grid_0.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/2417ee2c-39c1-4391-93dc-044ac6e0dcc9/0_3.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "A futuristic court jester playing a lute, neon lights, ambient lighting, smooth lighting, cartoon",
+      date: "4/13/2023"
   },
   {
-    link: "https://cdn.midjourney.com/71eb021d-17a0-4077-8e89-ce492a07a425/grid_0.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/2b4e2d13-d96d-4e4a-b9e5-a43bfe6e8021/0_3.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "A short and round mech with ears like a cat, the mech walks on two legs and two arms attached to a round body with cat ears, anime style, smooth lighting, dark background",
+      date: "4/12/2023"
   },
   {
-    link: "https://cdn.midjourney.com/fcee5f24-3134-4fe9-98df-0ac834935c82/grid_0.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/61a14704-1a09-4f45-aacd-0889a5ae0a3c/0_3.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "A cat sitting in the drivers seat in the cockpit of a cat shaped mech, the mech's head is shaped like a cat, the mech is bipedal, view looking in through the windshield, anime style, smooth lighting",
+      date: "4/12/2023"
   },
   {
-    link: "https://cdn.midjourney.com/1838b2d0-5385-46d3-8aa0-0d0638a73e75/grid_0.webp",
-    ar: "1:2",
-    imgType: "tallImage",
+      link: "https://cdn.midjourney.com/3dea0bf8-b0cc-47a3-9171-104f857186e8/0_0.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "A cat driving a cat shaped bipedal mech, anime style, smooth lighting",
+      date: "4/11/2023"
   },
+  {
+      link: "https://cdn.midjourney.com/cee9d78c-8eea-495b-88a2-224232ee32a1/0_1.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "A duck standing next to a pond, smooth lighting, photorealistic",
+      date: "4/3/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/4d120f9d-e707-4ffb-a9bf-aad5223bf3b7/0_3.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "Closeup of fish in a fishtank filled with aquarium - decorations, side view, cartoon, Where's Waldo art",
+      date: "4/2/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/a5536a31-e27a-43c2-a4c5-c927b96023f5/0_2.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "A school of multicolored goldish swimming away from the open jaws of a massive whale shark, deep sea, vibrant, digital art",
+      date: "4/2/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/b6c1e9d7-b04d-49b0-a146-2d02655a5a28/0_1.webp",
+      version: 5,
+      ar: "1:1",
+      size: "1024x1024",
+      imgType: "squareImage",
+      text: "A chubby brown bear wearing loose samurai armor holding a naginata, digital art",
+      date: "4/2/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/18d1dcc4-e39e-43f5-932f-66824e37bda6/0_3.webp",
+      version: 5,
+      ar: "1:2",
+      size: "768x1536",
+      imgType: "tallImage",
+      text: "Medieval knight wearing psychedelic armor, portrait, painting, smooth lighting",
+      date: "3/31/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/2ba9fea3-77f3-4b33-a556-bf40e5ab445a/grid_0.webp",
+      version: 4,
+      ar: "1:2",
+      size: "864x1728",
+      imgType: "tallImage",
+      text: "In the process of developing machines that are conscious and moral agents, we will be inevitably creating billions of entities that are capable of suffering. And we will inevitably inflict suffering on them. , Digital art",
+      date: "3/30/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/fcee5f24-3134-4fe9-98df-0ac834935c82/grid_0.webp",
+      version: 4,
+      ar: "1:2",
+      size: "864x1728",
+      imgType: "tallImage",
+      text: "Skeleton of a 60ft giant man being displayed as the main exhibit in a museum, held up with wire support, digital art, by Arthur Adams",
+      date: "3/22/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/1838b2d0-5385-46d3-8aa0-0d0638a73e75/grid_0.webp",
+      version: 4,
+      ar: "1:2",
+      size: "864x1728",
+      imgType: "tallImage",
+      text: "Preserved eyeball sitting at the base of a glass jar filled with green viscous fluid, in an old laboratory, cinematic, detailed",
+      date: "3/16/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/d11a8775-3920-4713-9c4c-90d095bcdfe7/grid_0.webp",
+      version: 4,
+      ar: "1:2",
+      size: "864x1728",
+      imgType: "tallImage",
+      text: "Skull icon, black skull, white background, pop art",
+      date: "3/16/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/71eb021d-17a0-4077-8e89-ce492a07a425/grid_0.webp",
+      version: 4,
+      ar: "1:2",
+      size: "864x1728",
+      imgType: "tallImage",
+      text: "Demon with pillar of white smoke for a head, detailed, cartoon",
+      date: "3/14/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/e4e800c8-22ef-4b13-9e87-1224af9003c2/grid_0.webp",
+      version: 4,
+      ar: "1:2",
+      size: "864x1728",
+      imgType: "tallImage",
+      text: "Gravestones in small graveyard, pop art",
+      date: "3/14/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/ae310129-9086-474e-8417-429931bf3b25/grid_0.webp",
+      version: 4,
+      ar: "1:2",
+      size: "864x1728",
+      imgType: "tallImage",
+      text: "Rainbow fire and magma flowing off floating orb, bright colors, digital art",
+      date: "3/9/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/ea3d651c-a3d0-4cf4-8b3f-16e3a6cd0421/grid_0.webp",
+      version: 4,
+      ar: "1:2",
+      size: "864x1728",
+      imgType: "tallImage",
+      text: "Logo of water dripping off a floating orb, mono - colored, digital art",
+      date: "3/9/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/bf03dc12-62db-49d3-abfa-bb49b8e6246a/grid_0.webp",
+      version: 4,
+      ar: "1:2",
+      size: "1024x2048",
+      imgType: "tallImage",
+      text: "Desert planet with thin atmosphere, stars in sky, Unreal Engine, 3D Render",
+      date: "3/9/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/df4c573f-65b9-4ff9-9b72-39d3a92aa73d/grid_0.webp",
+      version: 4,
+      ar: "1:2",
+      size: "864x1728",
+      imgType: "tallImage",
+      text: "Museum exhibit covered in assorted ornate ancient masks, Unreal Engine, 3D Render",
+      date: "3/9/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/0a2c1651-f70b-431d-8ffb-4325231827ed/grid_0.webp",
+      version: 4,
+      ar: "1:2",
+      size: "1024x2048",
+      imgType: "tallImage",
+      text: "Swarm of bees, scientific diagram, colored pencil",
+      date: "3/6/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/fb4f6f1a-1856-4dad-bf40-1066a02bcc11/grid_0.webp",
+      version: 4,
+      ar: "1:2",
+      size: "864x1728",
+      imgType: "tallImage",
+      text: "An octopus in the deep blue ocean, Illustration, by Wayne Barlowe",
+      date: "2/22/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/4b04eb86-4bb8-4c26-b8cd-24374975e5a8/grid_0.webp",
+      version: 4,
+      ar: "2:3",
+      size: "1024x1536",
+      imgType: "twoThirdsImage",
+      text: "A panther prowling camouflaged in the jungle underbrush, digital art, detailed",
+      date: "2/21/2023"
+  },
+  {
+      link: "https://cdn.midjourney.com/8c7ba7ba-bd6c-4877-b621-c668e28ff6c2/grid_0.webp",
+      version: 4,
+      ar: "2:3",
+      size: "1024x1536",
+      imgType: "twoThirdsImage",
+      text: "Demon animal with large horns, smoldering fur coat, glowing red eyes, photography",
+      date: "2/21/2023"
+  }
 ];
 
 /*
